d5213b34f369e1be1bda3f9c53b8fb35cb2330d0
Jesus <jh.ledesmaa@gmail.com>
2021-06-12 22:12:13 -0300

    createPerson

--- src/main/kotlin/com/demo/cleanarchitecturecoroutines/core/person/service/PersonService.kt
+++ src/main/kotlin/com/demo/cleanarchitecturecoroutines/core/person/service/PersonService.kt
@@ -1,13 +1,21 @@
 package com.demo.cleanarchitecturecoroutines.core.person.service
 
+import com.demo.cleanarchitecturecoroutines.core.book.service.BookService
+import com.demo.cleanarchitecturecoroutines.core.film.service.FilmService
 import com.demo.cleanarchitecturecoroutines.core.person.model.Person
 import com.demo.cleanarchitecturecoroutines.core.person.model.PersonInput
+import com.demo.cleanarchitecturecoroutines.repository.PersonRepository
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
 
-class PersonService {
+class PersonService(private val bookService: BookService, private val filmService: FilmService, private val personRepository: PersonRepository){
 
-    suspend fun createPerson(personInput: PersonInput): Person {
+    suspend fun createPerson(personInput: PersonInput): Person = coroutineScope {
+        val bookAsync = async { bookService.getBook(personInput.isbn) }
+        val filmAsync = async { filmService.getFilm(personInput.title) }
 
+        personRepository.save(Person(personInput.dni, personInput.name, bookAsync.await(), filmAsync.await()))
     }
 
     suspend fun getPerson(dni: Long): Person? {
