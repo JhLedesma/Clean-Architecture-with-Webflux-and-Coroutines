84ce7dc0179724999e79aa7431caa468b1b89486
Jesus <jh.ledesmaa@gmail.com>
2021-06-24 01:09:27 -0300

    move controller spring

--- src/main/kotlin/com/demo/cleanarchitecturecoroutines/app/run/SpringRun.kt
+++ src/main/kotlin/com/demo/cleanarchitecturecoroutines/app/run/SpringRun.kt
@@ -7,10 +7,10 @@ import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRep
 
 @SpringBootApplication
 @EnableReactiveMongoRepositories("com.demo.cleanarchitecturecoroutines.repository")
-//@EntityScan("repository")
 @ComponentScan(basePackages = ["com.demo.cleanarchitecturecoroutines"])
 class SpringRun
 
 fun main(args: Array<String>) {
     runApplication<SpringRun>(*args)
 }
+
